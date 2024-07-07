ddc231bb9474f6ffddb9e3fc2c548f1ab26aa4be
The.First.Step <46297678+AnijaaaPan@users.noreply.github.com>
2024-07-07 19:49:54 +0900

    feat: ファビコンを動的に設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Favicon from '../../public/favicon.ico';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   description: '都道府県別 推移グラフを動的に生成出来ます',
-  icons: {
-    icon: '/favicon.ico',
-  },
+  icons: [{ rel: 'icon', url: Favicon.src }],
   title: '都道府県別 推移グラフ',
 };
 
