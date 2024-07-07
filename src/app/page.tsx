8bef64219004add2a5a5e23677f0158272159d39
The.First.Step <46297678+AnijaaaPan@users.noreply.github.com>
2024-07-07 17:51:39 +0900

    feat: グラフのデータを取得し、表示する関数を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,63 @@
-import { CheckBox, HandleClick } from '@/features/CheckBox';
-import GetPrefectures from '@/getters/GetPrefectures';
+'use client';
 
-export default async function Home() {
-  const prefectures = await GetPrefectures();
+import Graph from '@/app/components/Graph';
+import { PrefecturesData, PrefPopulationData } from '@/interfaces/IResas';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { CheckBoxPrefectures } from './components/CheckBoxPrefectures';
+import { CheckBoxPrefPopulation } from './components/CheckBoxPrefPopulation';
+
+export default function Home() {
+  const [prefectures, setPrefectures] = useState<PrefecturesData[]>([]);
+  const [prefPopulation, setPrefPopulation] = useState<PrefPopulationData[]>([]);
+  const labels = ['総人口', '年少人口', '生産年齢人口', '老年人口'];
+  const [labelIndex, setLabel] = useState<number>(0);
+
+  useEffect(() => {
+    axios
+      .get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY },
+      })
+      .then((results) => setPrefectures(results.data.result))
+      .catch(() => {});
+  }, []);
+
+  const HandleCheckBoxLabel = async (index: number) => {
+    setLabel(index);
+  };
+
+  const HandleCheckBoxPrefectures = async (
+    prefCode: number,
+    prefName: string,
+    isChecked: boolean
+  ) => {
+    if (isChecked) {
+      if (prefPopulation.some((pop) => pop.prefName === prefName)) return;
+
+      await axios
+        .get(
+          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`,
+          { headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY } }
+        )
+        .then((results) => {
+          setPrefPopulation([...prefPopulation, { prefName, data: results.data.result.data }]);
+        })
+        .catch(() => {});
+    } else {
+      setPrefPopulation(prefPopulation.filter((pop) => pop.prefName !== prefName));
+    }
+  };
 
   return (
     <main>
       <h2>都道府県</h2>
-      {prefectures && <CheckBox prefectures={prefectures} onChange={HandleClick} />}
+      <CheckBoxPrefectures prefectures={prefectures} onChange={HandleCheckBoxPrefectures} />
+
+      <h2>推移タイプ</h2>
+      <CheckBoxPrefPopulation labels={labels} onChange={HandleCheckBoxLabel} />
+
+      <h2>グラフ: {labels[labelIndex]}推移</h2>
+      <Graph labels={labels} labelIndex={labelIndex} prefPopulationDatas={prefPopulation} />
     </main>
   );
 }
