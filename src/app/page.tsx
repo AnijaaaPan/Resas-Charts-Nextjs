170cd745ff2744dff3c62632528b80a1a8002884
The.First.Step <46297678+AnijaaaPan@users.noreply.github.com>
2024-07-08 14:27:06 +0900

    feat: 適切な粒度でコンポーネントを分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { useState } from 'react';
-
 import PopulationGraph from '@/app/components/PopulationGraph';
 import { PopulationTypeRadioGroup } from '@/app/components/PopulationTypeRadioGroup';
 import { PrefectureCheckBoxGroup } from '@/app/components/PrefectureCheckBoxGroup';
-import { useFetchPrefectures } from '@/app/hooks/useFetchPrefectures';
-import { useFetchPrefPopulation } from '@/app/hooks/useFetchPrefPopulation';
-
-const populationLabels = ['総人口', '年少人口', '生産年齢人口', '老年人口'];
+import { usePageLogic } from './hooks/usePageLogic';
 
 export default function Home() {
-  const prefectures = useFetchPrefectures();
-  const { prefPopulation, handleCheckBoxPrefectures } = useFetchPrefPopulation();
-  const [labelIndex, setLabel] = useState<number>(0);
+  const {
+    handleCheckBoxPrefectures,
+    handlePopulationTypeChange,
+    populationLabels,
+    prefectures,
+    prefPopulation,
+    selectedPopulationType,
+  } = usePageLogic();
 
   return (
     <main>
@@ -24,14 +24,14 @@ export default function Home() {
 
       <section>
         <h1>推移タイプ</h1>
-        <PopulationTypeRadioGroup labels={populationLabels} onChange={setLabel} />
+        <PopulationTypeRadioGroup labels={populationLabels} onChange={handlePopulationTypeChange} />
       </section>
 
       <section>
         <h1>グラフ</h1>
         <PopulationGraph
           labels={populationLabels}
-          labelIndex={labelIndex}
+          labelIndex={selectedPopulationType}
           prefPopulationDatas={prefPopulation}
         />
       </section>
